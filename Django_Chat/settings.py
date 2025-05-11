b411dfcf0a2c295c70ffdf5bd40b8306d7dad24a
sanjivthapa <thapasvt12@gmail.com>
2025-05-11 19:42:35 +0545

    Done

--- Django_Chat/settings.py
+++ Django_Chat/settings.py
@@ -172,7 +172,7 @@ USE_I18N = True
 USE_TZ = True
 
 # Static files configuration
-STATIC_URL = '/static'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
