687163633418c5a504a2bc1debb050f26797b79d
sanjivthapa <thapasvt12@gmail.com>
2025-05-13 15:08:09 +0545

    hey yoo

--- user_api/urls.py
+++ user_api/urls.py
@@ -1,13 +1,12 @@
 from django.urls import path, include
-from .views import LoginView, RegisterView, LogoutView, UserProfileView, FriendRequestViewSet, FriendViewSet,ListUserViewSet
+from .views import LoginView, RegisterView, LogoutView, UserProfileView, FriendRequestViewSet, FriendModelViewSet,ListUserViewSet
 from rest_framework.routers import DefaultRouter
 from .serializers import MyTokenRefreshSerializer
 from rest_framework_simplejwt.views import TokenRefreshView
 router = DefaultRouter()
 router.register(r'friend-requests', FriendRequestViewSet, basename='friend_requests')
-router.register(r'friends', FriendViewSet, basename='friends')
+router.register(r'friends', FriendModelViewSet, basename='friends')
 router.register(r'users', ListUserViewSet, basename='users')
-
 #for jwt to send username when getting access token with refresh tiken
 class MyTokenRefreshView(TokenRefreshView):
     serializer_class = MyTokenRefreshSerializer
